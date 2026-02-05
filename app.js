d2bb935241aa26f13f37a0b6ffee5f738e971ec1
Simone <info@simoneicardi.com>
2026-02-05 11:28:31 +0100

    rendi disponibili file statici su public

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+// attivazione della cartella public per uso file statici
+app.use(express.static('public'));
+
 app.get('/', (req, res) => {
 
     // creo un oggetto js da inviare nella res
     const person = {
         name: "Ted",
-        lastname: "Lasso"
+        lastname: "Lasso",
+        img: "http://localhost:3000/logo-boolean.jpg"
     };
 
     res.json(person);
