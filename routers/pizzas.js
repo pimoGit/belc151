c6259d8ad477ea1ad46b04394a8fcbc885bb549e
Simone <info@simoneicardi.com>
2026-02-09 10:35:12 +0100

    rotta copleta index con aggiunta filtro ingrediente

--- routers/pizzas.js
+++ routers/pizzas.js
@@ -10,10 +10,20 @@ const menuPizze = require('./../data/menu');
 // index
 router.get('/', function (req, res) {
 
+    //Inizialmente, il menu filtrato corrisponde a quello originale
+    let filteredMenu = menuPizze;
+
+    // Se la richiesta contiene un filtro, allora filtriamo il menu
+    if (req.query.ingredient) {
+        filteredMenu = menuPizze.filter(
+            pizza => pizza.ingredients.includes(req.query.ingredient)
+        );
+    }
+
     // creo un nuovo oggetto con le prop che mi servono
     const oggettoMenu = {
-        numeroPizze: menuPizze.length,
-        listaPizze: menuPizze
+        numeroPizze: filteredMenu.length,
+        listaPizze: filteredMenu
     };
 
     // restituisco in risposta ql'oggetto creato
